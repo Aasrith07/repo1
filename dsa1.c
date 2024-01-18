43685bef819724c93df33473b3fc87c4b2b94282
Aasrith07 <155884013+Aasrith07@users.noreply.github.com>
2024-01-18 09:12:22 +0000

    Pending changes exported from your codespace

---
+++ dsa1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+ int main()
+ {
+    // int a,b,c;
+    // int array1[10];
+
+    // for(int i=0;i<=9;i++){
+
+    //     array1[i] = i*i;
+    // }
+    // for(int i=0;i<=9;i++){
+
+    //     printf("The array is: ", &array1[i]);
+    // }
+
+   //  int c;
+
+   //  c = (95/88);
+   //  printf("%i",c);
+// int myNumbers[4] = {25, 50, 75, 100};
+// int i;
+
+// for (i = 0; i < 4; i++) {
+//   printf("%p\n", &myNumbers[i]);
+// }
+
+struct student{
+   int rno;
+   char name[80];
+   float fees;
+   char dob[80]; 
+};
+
+struct student stud1;
+scanf("%c",&stud1.name);
+printf("%c",stud1.name);
+return 0;
+ }
---
+++ waterfil.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+	int chef[3],sum=0;
+	int testcases;
+    
+    scanf("%d",&testcases);
+	
+    for(int j = 0;j<testcases; j++){
+		
+    	scanf("%d %d %d" ,&chef[0],&chef[1],&chef[2]);
+	
+    	for(int i=0;i<3;i++){
+		    sum += chef[i];
+		}
+
+		if(sum < 2){
+		    printf("Water filling time\n");
+		}
+		else{
+		    printf("Not now\n");
+		}	
+    }
+	return 0;
+}
